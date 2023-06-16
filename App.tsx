9979746af91822b747c380c440a39743b32f94d9
victorasth <victorasth@gmail.com>
2023-06-15 22:44:06 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from 'react-native';
+import { styles } from './styles';
+
+
+
+const App = () => {
+    return <View style={styles.container}>
+        <Text styles={styles.title}>teste</Text>
+    </View>;
+};
+ export default App;
+
